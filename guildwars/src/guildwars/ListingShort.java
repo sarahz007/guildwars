4f068dc7e0b3044db836c357904acc2bdae32f6e
Sarah Ziegler <sarahz@nexum.de>
2015-11-11 08:30:31 +0100

    now also claculating min, max, median and average Unit Price

--- guildwars/src/guildwars/ListingShort.java
+++ guildwars/src/guildwars/ListingShort.java
@@ -40,18 +40,71 @@ public class ListingShort {
 		return results;
 	}
 	
-	public static String outputList(int[] results){
+	public static List<Listing> outputList(int[] results){
 		List<Listing> finalListing = new ArrayList<>();
 		for (int a = 2; a < results.length; a = a+3){
 			Listing listObject = new Listing(results[a], results[a-1]);
 			finalListing.add(listObject);
 		}
+		/*
 		for (int c = 0; c < finalListing.size(); c++){
-			String object = finalListing.get(c).getQuantity() + "x " + (finalListing.get(c).getUnitPrice());
+			String object = finalListing.get(c).getQuantity() + "x " + finalListing.get(c).getUnitPrice();
 			System.out.println(object);;
 		}
-		return null;
+		*/
+		return finalListing;
 	}
+	
+	public static int minimum(List<Listing> finalListing){
+		List<Integer> uP = new ArrayList<>();
+		for (int i = 0; i < finalListing.size(); i++){
+			
+			uP.add(finalListing.get(i).getUnitPrice());
+		}
+		int min =Collections.min(uP);
+		System.out.println("min: " + min);
+		return min;	
+	}
+	
+	public static int maximum(List<Listing> finalListing){
+		List<Integer> uP = new ArrayList<>();
+		for (int i = 0; i < finalListing.size(); i++){
+			
+			uP.add(finalListing.get(i).getUnitPrice());
+		}
+		int max = Collections.max(uP);
+		System.out.println("max: " + max);
+		return max;
+	}
+	
+	public static int median (List<Listing> finalListing){
+		int[] up = new int[finalListing.size()];
+		for(int i = 0; i < finalListing.size(); i++){
+			up[i] = finalListing.get(i).getUnitPrice();
+		}
+		int median;
+		Arrays.sort(up);
+		if (up.length % 2 == 0)
+			median = up[up.length/2] + up[up.length/2 - 1];
+		else median = up[up.length/2];
+		System.out.println("median: " + median);
+		return median;
+	}
+	
+	public static double average (List<Listing> finalListing){
+		int all = 0;
+		for (int i = 0; i < finalListing.size(); i++){
+			all += finalListing.get(i).getUnitPrice();
+		}
+		double avg = all/finalListing.size();
+		System.out.println("average: " + avg);
+		return avg;
+	}
+	
+	
+	//public int calculateMaximum
+	//public int calculateAverage
+	
 	
 	public static void main(String[] args) throws Exception {
 		String values = loadUrl("https://api.guildwars2.com/v2/commerce/listings/19684");
@@ -61,8 +114,16 @@ public class ListingShort {
 		
 		System.out.println("Buy: " + "\n");
 		outputList(createList(buy));
+		minimum(outputList(createList(buy)));
+		maximum(outputList(createList(buy)));
+		median(outputList(createList(buy)));
+		average(outputList(createList(buy)));
 		System.out.println("\n" + "Sell: " + "\n");
 		outputList(createList(sell));
-		
+		minimum(outputList(createList(sell)));
+		maximum(outputList(createList(sell)));
+		median(outputList(createList(sell)));
+		average(outputList(createList(sell)));
 	}
+	
 }
